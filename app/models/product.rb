d2ce86dd2dbf967e71132a9a873d94209175e414
ivanboda <ivan3128@mail.ru>
2017-03-24 00:20:48 -0700

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,18 @@ class Product < ApplicationRecord
 	with: %r{\.(gif|jpg|png)\Z}i,
 	message: 'URL of image is not corect'
 	}
+	def self.latest
+	Product.order(:updated_at).last
+	end
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	private
+	def ensure_not_referenced_by_any_line_item
+	if line_items.empty?
+	return true
+	else
+	errors.add(:base, 'There are comoditi positions')
+	return false
+	end
+	end
 end
