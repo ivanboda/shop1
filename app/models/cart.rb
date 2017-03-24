30b9e7c3cd823e9541270d9f60106033d3423a7d
ivanboda <ivan3128@mail.ru>
2017-03-24 14:51:23 -0700

    debug

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,5 @@
 class Cart < ApplicationRecord
+	has_many :line_items, dependent: :destroy
 	def add_product(product_id)
 	current_item = line_items.find_by(product_id: product_id)
 	if current_item
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
@@ -9,8 +11,6 @@ class Product < ApplicationRecord
 	def self.latest
 	Product.order(:updated_at).last
 	end
-	has_many :line_items
-	before_destroy :ensure_not_referenced_by_any_line_item
 	private
 	def ensure_not_referenced_by_any_line_item
 	if line_items.empty?
