3d0ec36a62373ab0bdfceea5487da07301e00b0b
ivanboda <ivan3128@mail.ru>
2017-03-24 08:05:32 -0700

    cart2

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,12 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
-
+rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
   # GET /carts
   # GET /carts.json
+  def invalid_cart
+logger.error "Attempt to access invalid cart #{params[:id]}"
+redirect_to store_url, notice: 'Invalid cart'
+end
   def index
     @carts = Cart.all
   end
@@ -54,10 +58,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
-    respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.json { head :no_content }
+   @cart.destroy if @cart.id == session[:cart_id]
+session[:cart_id] = nil
+respond_to do |format|
+format.html { redirect_to store_url,
+notice: 'Тепер ваша корзина пуста!' }
+format.json { head :no_content }
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324063629) do
+ActiveRecord::Schema.define(version: 20170324141754) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
